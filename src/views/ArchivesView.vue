188647e705cc6c8b39a170d7c2cdac2f7e778491
Viraj Khatavkar <viraj.2438@gmail.com>
2022-07-31 23:32:27 +0530

    Add archive#1 link

--- src/views/ArchivesView.vue
+++ src/views/ArchivesView.vue
@@ -6,7 +6,7 @@
     <div class="mb-4">
         <ul>
             <li>
-                <a href="/" class="text-blue-700">
+                <a href="https://winning-author-4379.ck.page/posts/phpjsweekly-com-newsletter-1-rest-hooks-traits-vue-composables-guzzle-and-much-more" class="text-blue-700" target="_blank">
                     Archive #1
                 </a>
                 - 31st July, 2022
